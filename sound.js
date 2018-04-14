4ff1e9222dc30cb72ca1a16deb30f7487b5c92ef
Daehoon Kim <ghsdh3409@gmail.com>
2018-04-14 12:41:16 +0900

    Make setSpeakerphoneOn public for iOS

--- sound.js
+++ sound.js
@@ -279,8 +279,6 @@ Sound.prototype.setCurrentTime = function(value) {
 Sound.prototype.setSpeakerphoneOn = function(value) {
   if (IsAndroid) {
     RNSound.setSpeakerphoneOn(this._key, value);
-  } else if (!IsAndroid && !IsWindows) {
-    RNSound.setSpeakerphoneOn(value);
   }
 };
 
@@ -359,6 +357,12 @@ Sound.setActive = function(value) {
 Sound.setCategory = function(value, mixWithOthers = false, allowBluetooth = false) {
   if (!IsAndroid && !IsWindows) {
     RNSound.setCategory(value, mixWithOthers, allowBluetooth);
+  }
+};
+
+Sound.setSpeakerphoneOn = function(value) {
+  if (!IsAndroid && !IsWindows) {
+    RNSound.setSpeakerphoneOn(value);
   }
 };
 
